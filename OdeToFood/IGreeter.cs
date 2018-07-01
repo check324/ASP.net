8f83e5196e776c188b3648a1b7b14d7ef020160c
Tianyi Jiang <tianjiang@tesla.com>
2018-07-01 12:57:35 -0700

    using IApplicationBuilder

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood {
+    public interface IGreeter {
+        string GetMessageOfTheDa();
+    }
+
+    public class Greeter : IGreeter {
+        public string GetMessageOfTheDa() {
+            return _configuration["Greeting"];
+        }
+        public Greeter(IConfiguration configuration) => _configuration = configuration;
+        private IConfiguration _configuration;
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -15,19 +17,35 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.Use(next => {
+                return async context => {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym")) {
+                        await context.Response.WriteAsync("Hit the midware");
+                        logger.LogInformation("Request handled");
+                    } else {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+                };
+            });
+            app.UseWelcomePage(new WelcomePageOptions {
+                Path = "/wp"
+            });
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetMessageOfTheDa();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
